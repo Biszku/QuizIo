788bf009ec46041995274b2a8b2fb13fbcd8d436
Biszku <dbichu@onet.pl>
2023-08-07 22:03:18 +0200

    initially done preview

--- hooks/useGetQuzzies.ts
+++
@@ -1,9 +0,0 @@
-const useGetQuzzies = async (category: string, arrOfDifficulty: string[]) => {
-  const req = await fetch(
-    `https://quizapi.io/api/v1/questions?apiKey=${process.env.API_KEY}&difficulty=easy`
-  );
-  const res = await req.json();
-  return res;
-};
-
-export default useGetQuzzies;
