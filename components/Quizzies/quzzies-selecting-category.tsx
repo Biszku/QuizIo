55605334ae22bc26a2a013d9d27fcffb9a8ef3d3
Biszku <dbichu@onet.pl>
2023-08-09 13:57:10 +0200

    edited animation delay

--- components/Quizzies/quzzies-selecting-category.tsx
+++ components/Quizzies/quzzies-selecting-category.tsx
@@ -2,11 +2,15 @@ const QuizziesSelectingCategory = ({
   category,
   delay,
   visibility,
+  turnOffCategories,
 }: {
   category: string;
   delay: number;
   visibility: boolean;
+  turnOffCategories: () => void;
 }) => {
+  const input = document.querySelector("#showCategories")! as HTMLInputElement;
+
   return (
     <div
       className={`general-container_selecting-container_loc-item ${
@@ -14,7 +18,11 @@ const QuizziesSelectingCategory = ({
       }`}
       style={{
         backgroundImage: `linear-gradient(to bottom, rgba(245, 246, 252, 0.52), rgba(255, 146, 43, 0.73)),url(/${category}.jpg)`,
-        animationDelay: `${0.1 * delay}s`,
+        animationDelay: `${0.05 * delay}s`,
+      }}
+      onClick={() => {
+        input.checked = false;
+        turnOffCategories();
       }}
     >
       <span className="general-container_selecting-container_loc-item-category">{`${category}`}</span>
