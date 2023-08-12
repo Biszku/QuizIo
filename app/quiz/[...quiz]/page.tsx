b6bba8b1da38a125bdf59b3090e6cd4070d89736
Biszku <dbichu@onet.pl>
2023-08-12 20:36:31 +0200

    created a quiz page

--- app/quiz/[...quiz]/page.tsx
+++ app/quiz/[...quiz]/page.tsx
@@ -1,5 +1,17 @@
-const Quiz = () => {
-  return <h1>Quiz</h1>;
+import GetQuzzies from "../../../utils/getQuzzies";
+import { FC } from "react";
+import QuizContent from "../../../components/quiz/quiz-content";
+
+interface ParamsSlug {
+  params: {
+    quiz: string[];
+  };
 }
+
+const Quiz: FC<ParamsSlug> = async ({ params }) => {
+  const [category, difficult] = params.quiz;
+  const quiz = await GetQuzzies(category, difficult);
+  return <QuizContent data={quiz} />;
+};
 
 export default Quiz;
