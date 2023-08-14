e9d30dc2c3d73c79931ddb8a509f5597e371a62c
Biszku <dbichu@onet.pl>
2023-08-14 18:24:28 +0200

    added quiz element

--- components/quiz/quiz-content.tsx
+++ components/quiz/quiz-content.tsx
@@ -1,5 +1,6 @@
 import GetQuzzies from "../../utils/getQuzzies";
 import { FC } from "react";
+import SingleQuiz from "./quiz-single";
 
 interface parametersProps {
   parameters: {
@@ -10,13 +11,9 @@ interface parametersProps {
 const QuizContent: FC<parametersProps> = async ({ parameters }) => {
   const [category, difficult] = parameters.quiz;
   const quiz = await GetQuzzies(category, difficult);
-  
-  console.log(1, quiz);
   return (
     <section className="quiz_container">
-      {quiz.map((el: any) => {
-        return <div className="quiz_container_single-quiz">{el.id}</div>;
-      })}
+      <SingleQuiz arrOfQuizzies={quiz} />
     </section>
   );
 };
