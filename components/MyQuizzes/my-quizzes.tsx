c736955a27a9ed29de0029f9b20051802c155f5a
Biszku <dbichu@onet.pl>
2023-08-17 18:54:15 +0200

    added skeleton of component

--- components/MyQuizzes/my-quizzes.tsx
+++ components/MyQuizzes/my-quizzes.tsx
@@ -1,13 +1,27 @@
 "use client";
 import { useContext } from "react";
 import { MainContext } from "../../context/context";
+import Link from "next/link";
 
 const MyQuizzesSection = () => {
   const { quizzes } = useContext(MainContext);
-  console.log(quizzes);
   return (
     <div className="">
-      <h1>Elon</h1>
+      {quizzes.map((el, index) => (
+        <div key={index} className="">
+          <p>{el.category}</p>
+          <p>{el.difficulty}</p>
+          <Link
+            href={`${
+              el.status === "finished"
+                ? `/my-quizzies`
+                : `/quiz/${el.category}/${el.difficulty}`
+            }`}
+          >
+            <p>{el.status}</p>
+          </Link>
+        </div>
+      ))}
     </div>
   );
 };
