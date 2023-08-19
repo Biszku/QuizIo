af7b39f5ca71c853f61a0fbf7671f0e7d439c6af
Biszku <dbichu@onet.pl>
2023-08-19 23:46:49 +0200

    imported QuizInfo element

--- app/my-quizzies/quiz-info/[...id]/page.tsx
+++ app/my-quizzies/quiz-info/[...id]/page.tsx
@@ -1,5 +1,16 @@
-const MyQuizInfo = () => {
-  return <h1>Quiz Info</h1>;
+import QuizInfo from "@/components/Quiz-info/quiz--info";
+import { FC } from "react";
+
+interface ParamsSlug {
+  params: {
+    id: string[];
+  };
+}
+
+const MyQuizInfo: FC<ParamsSlug> = ({ params }) => {
+  const [category, difficulty] = params.id;
+
+  return <QuizInfo params={{ category, difficulty }} />;
 };
 
 export default MyQuizInfo;
