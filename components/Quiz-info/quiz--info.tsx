6973764b22a278990dc2be4fbe6ffa55b963c928
Biszku <dbichu@onet.pl>
2023-08-22 02:55:43 +0200

    created basic quiz-info container

--- components/Quiz-info/quiz--info.tsx
+++ components/Quiz-info/quiz--info.tsx
@@ -1,4 +1,9 @@
+"use client";
 import { FC } from "react";
+import { useContext } from "react";
+import { MainContext } from "@/context/context";
+import Link from "next/link";
+import { HiArrowSmLeft } from "react-icons/hi";
 
 interface ParamsSlug {
   params: {
@@ -8,8 +13,28 @@ interface ParamsSlug {
 }
 
 const QuizInfo: FC<ParamsSlug> = ({ params }) => {
+  const { quizzes } = useContext(MainContext);
   const { category, difficulty } = params;
-  return <h1>{`${category}, ${difficulty}`}</h1>;
+  const curQuiz = quizzes.find(
+    (el) => el.category === category && el.difficulty === difficulty
+  );
+  return (
+    <section
+      className="my-quizzes-info"
+      style={{
+        backgroundImage: `linear-gradient(to bottom, rgba(245, 246, 252, 0.52), rgba(245, 246, 252, 0.73)),url(/mini-${category}.jpg)`,
+      }}
+    >
+      <div className="my-quizzes-info_container">
+        <div className="my-quizzes-info_container-filler"></div>
+        <span>{`${category}`}</span>
+        <span>{`${difficulty}`}</span>
+        <Link href="/my-quizzies" className="my-quizzes-info_container-link">
+          <HiArrowSmLeft />
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default QuizInfo;
