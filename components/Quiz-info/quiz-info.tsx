0b34c17bf7cb7390682763d9c328b68bff62465a
Biszku <dbichu@onet.pl>
2023-08-23 21:28:12 +0200

    changed type of fetching data

--- components/Quiz-info/quiz-info.tsx
+++ components/Quiz-info/quiz-info.tsx
@@ -1,7 +1,4 @@
-"use client";
 import { FC } from "react";
-import { useContext } from "react";
-import { MainContext } from "@/context/context";
 import Link from "next/link";
 import { HiArrowSmLeft } from "react-icons/hi";
 
@@ -13,22 +10,17 @@ interface ParamsSlug {
 }
 
 const QuizInfo: FC<ParamsSlug> = ({ params }) => {
-  const { quizzes } = useContext(MainContext);
-  const { category, difficulty } = params;
-  const curQuiz = quizzes.find(
-    (el) => el.category === category && el.difficulty === difficulty
-  );
   return (
     <section
       className="my-quizzes-info"
       style={{
-        backgroundImage: `linear-gradient(to bottom, rgba(245, 246, 252, 0.52), rgba(245, 246, 252, 0.73)),url(/mini-${category}.jpg)`,
+        backgroundImage: `linear-gradient(to bottom, rgba(245, 246, 252, 0.52), rgba(245, 246, 252, 0.73)),url(/mini-${params.category}.jpg)`,
       }}
     >
       <div className="my-quizzes-info_container">
         <div className="my-quizzes-info_container-filler"></div>
-        <span>{`${category}`}</span>
-        <span>{`${difficulty}`}</span>
+        <span>{`${params.category}`}</span>
+        <span>{`${params.difficulty}`}</span>
         <Link href="/my-quizzies" className="my-quizzes-info_container-link">
           <HiArrowSmLeft />
         </Link>
