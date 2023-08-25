47a1934081800a49ef5ce99bfd3fb6ad4baace89
Biszku <dbichu@onet.pl>
2023-08-26 00:14:53 +0200

    added argument with timeToAnswer

--- components/quiz/quiz-content.tsx
+++ components/quiz/quiz-content.tsx
@@ -11,9 +11,27 @@ interface parametersProps {
 const QuizContent: FC<parametersProps> = async ({ parameters }) => {
   const [category, difficult] = parameters.quiz;
   const quiz = await GetQuzzies(category, difficult);
+
+  let timeToAnswer = 0;
+  switch (difficult) {
+    case "easy":
+      timeToAnswer = 30;
+      break;
+    case "medium":
+      timeToAnswer = 45;
+      break;
+    case "hard":
+      timeToAnswer = 60;
+      break;
+  }
+
   return (
     <section className="quiz_container">
-      <SingleQuiz arrOfQuizzies={quiz} info={{ category, difficult }} />
+      <SingleQuiz
+        arrOfQuizzies={quiz}
+        info={{ category, difficult }}
+        timeToAnswer={timeToAnswer}
+      />
     </section>
   );
 };
