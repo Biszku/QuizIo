b9cb1a5bdc5ccfffdd7b985557c266feecdb4a5c
Biszku <dbichu@onet.pl>
2023-08-26 23:31:41 +0200

    created quiz continue element

--- components/Quiz-info/quiz-info-continue.tsx
+++ components/Quiz-info/quiz-info-continue.tsx
@@ -1,7 +1,40 @@
-const ContinueElement = () => {
+import { FC } from "react";
+import Link from "next/link";
+interface QuizziesInfoType {
+  quizziesQuestions: any[];
+  numOfCurQuiz: number;
+  params: {
+    category: string;
+    difficulty: string;
+  };
+}
+
+const ContinueElement: FC<QuizziesInfoType> = ({
+  quizziesQuestions,
+  numOfCurQuiz,
+  params,
+}) => {
+  const Questions = quizziesQuestions;
   return (
     <>
-      <p>continue</p>
+      <div className="my-quizzes-info_container_info-container_unfinished-container">
+        <span className="my-quizzes-info_container_info-container_unfinished-container-cur-question">
+          {`${numOfCurQuiz + 1}`}
+          <span className="my-quizzes-info_container_info-container_unfinished-container-cur-question-span">
+            current question
+          </span>
+        </span>
+        <span className="my-quizzes-info_container_info-container_unfinished-container-on">
+          on
+        </span>
+        <span className="my-quizzes-info_container_info-container_unfinished-container-total-questions">{`${Questions.length}`}</span>
+      </div>
+      <Link
+        className="my-quizzes-info_container_info-container-link btn"
+        href={`/quiz/${params.category}/${params.difficulty}`}
+      >
+        Continue
+      </Link>
     </>
   );
 };
