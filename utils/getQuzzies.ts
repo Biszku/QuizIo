8ffb857db68c7373f05beceb0fac0d43d1a9a946
Biszku <dbichu@onet.pl>
2023-08-31 19:26:04 +0200

    added no store cache data

--- utils/getQuzzies.ts
+++ utils/getQuzzies.ts
@@ -8,7 +8,8 @@ const GetQuzzies = async (
       process.env.NEXT_PUBLIC_API_KEY
     }&category=${
       category === "random" ? "" : category
-    }&difficulty=${difficulty}&limit=${limit}`
+    }&difficulty=${difficulty}&limit=${limit}`,
+    { cache: "no-store" }
   );
   const res = await req.json();
   return res;
