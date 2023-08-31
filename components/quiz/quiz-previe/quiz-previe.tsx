0c49de6125596d8a202db2f03de06c06f136dc83
Biszku <dbichu@onet.pl>
2023-08-31 21:11:05 +0200

    added backdrop filter element

--- components/quiz/quiz-previe/quiz-previe.tsx
+++ components/quiz/quiz-previe/quiz-previe.tsx
@@ -26,55 +26,61 @@ const QuizPrevie: FC<ParamsSlug> = async ({ params }) => {
       break;
   }
   return (
-    <section
-      className="quiz-previe-container"
-      style={{
-        backgroundImage: ` linear-gradient(to right bottom, rgba(200, 200, 200, .3), rgba(200, 200, 200, .4)),url(/mini-${params.category}.jpg)`,
-      }}
-    >
-      {/* <Image
+    <>
+      <section
+        className="quiz-previe-container"
+        style={{
+          backgroundImage: ` linear-gradient(to right bottom, rgba(200, 200, 200, .3), rgba(200, 200, 200, .4)),url(/mini-${params.category}.jpg)`,
+        }}
+      >
+        {/* <Image
         src={`/${params.category}.jpg`}
         width={300}
         height={400}
         className="quiz-previe-container-background"
         alt={params.category}
       ></Image> */}
-      <div className="quiz-previe-container_content">
-        <p className="quiz-previe-container_content-category">
-          {params.category}
-        </p>
-        <ul className="quiz-previe-container_content-list">
-          <li className="quiz-previe-container_content-list-item">
-            <HiChevronRight className="quiz-previe-container_content-list-item-icon" />
-            <span className="quiz-previe-container_content-list-item-prop">
-              Difficulty:
-            </span>
-            <span className="quiz-previe-container_content-list-item-param">{`${params.difficulty}`}</span>
-          </li>
-          <li className="quiz-previe-container_content-list-item">
-            <HiChevronRight className="quiz-previe-container_content-list-item-icon" />
+        <div className="quiz-previe-container_content">
+          <p className="quiz-previe-container_content-category">
+            {params.category}
+          </p>
+          <ul className="quiz-previe-container_content-list">
+            <li className="quiz-previe-container_content-list-item">
+              <HiChevronRight className="quiz-previe-container_content-list-item-icon" />
+              <span className="quiz-previe-container_content-list-item-prop">
+                Difficulty:
+              </span>
+              <span className="quiz-previe-container_content-list-item-param">{`${params.difficulty}`}</span>
+            </li>
+            <li className="quiz-previe-container_content-list-item">
+              <HiChevronRight className="quiz-previe-container_content-list-item-icon" />
 
-            <span className="quiz-previe-container_content-list-item-prop">
-              Time to answer:
-            </span>
-            <span className="quiz-previe-container_content-list-item-param">{`${timeToAnswer} sec`}</span>
-          </li>
-          <li className="quiz-previe-container_content-list-item">
-            <HiChevronRight className="quiz-previe-container_content-list-item-icon" />
+              <span className="quiz-previe-container_content-list-item-prop">
+                Time to answer:
+              </span>
+              <span className="quiz-previe-container_content-list-item-param">{`${timeToAnswer} sec`}</span>
+            </li>
+            <li className="quiz-previe-container_content-list-item">
+              <HiChevronRight className="quiz-previe-container_content-list-item-icon" />
 
-            <span className="quiz-previe-container_content-list-item-prop">
-              number of questions:
-            </span>
-            <span className="quiz-previe-container_content-list-item-param">
-              {numOfQuestions.length === 0 ? "0" : numOfQuestions.length}
-            </span>
-          </li>
-        </ul>
-        <StartQuizButton
-          params={{ category: params.category, difficulty: params.difficulty }}
-        />
-      </div>
-    </section>
+              <span className="quiz-previe-container_content-list-item-prop">
+                number of questions:
+              </span>
+              <span className="quiz-previe-container_content-list-item-param">
+                {numOfQuestions.length === 0 ? "0" : numOfQuestions.length}
+              </span>
+            </li>
+          </ul>
+          <StartQuizButton
+            params={{
+              category: params.category,
+              difficulty: params.difficulty,
+            }}
+          />
+        </div>
+        <div className="quiz-previe-container--backdrop-filter"></div>
+      </section>
+    </>
   );
 };
 
