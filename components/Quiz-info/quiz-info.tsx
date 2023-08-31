f5d3132a674337044b438857ac2d1652e063e1db
Biszku <dbichu@onet.pl>
2023-08-31 19:25:33 +0200

    added fetching quiz logic to element

--- components/Quiz-info/quiz-info.tsx
+++ components/Quiz-info/quiz-info.tsx
@@ -1,8 +1,11 @@
-import { FC } from "react";
+"use client";
+import { useContext, FC } from "react";
 import Link from "next/link";
 import { HiArrowSmLeft } from "react-icons/hi";
 import StatusElement from "./quiz-info-status";
 import InfoContainer from "./quiz-info-container";
+import { MainContext } from "../../context/context";
+import QuizSummary from "../quiz/quiz-summary";
 
 interface ParamsSlug {
   params: {
@@ -11,6 +14,15 @@ interface ParamsSlug {
   };
 }
 const QuizInfo: FC<ParamsSlug> = ({ params }) => {
+  const Context = useContext(MainContext);
+
+  const curQuiz = Context.quizzes.find(
+    (el) =>
+      el.category === params.category && el.difficulty === params.difficulty
+  );
+
+  console.log(curQuiz);
+
   return (
     <section
       className="my-quizzes-info"
@@ -18,30 +30,40 @@ const QuizInfo: FC<ParamsSlug> = ({ params }) => {
         backgroundImage: `linear-gradient(to bottom, rgba(245, 246, 252, 0.52), rgba(245, 246, 252, 0.73)),url(/mini-${params.category}.jpg)`,
       }}
     >
-      <div className="my-quizzes-info_container">
-        <div className="my-quizzes-info_container_core-info-container">
-          <span className="my-quizzes-info_container_core-info-container-category">{`${params.category}`}</span>
-          <span className="my-quizzes-info_container_core-info-container-difficulty">{`${params.difficulty}`}</span>
+      {curQuiz?.status === "unfinished" && (
+        <div className="my-quizzes-info_container">
+          <div className="my-quizzes-info_container_core-info-container">
+            <span className="my-quizzes-info_container_core-info-container-category">{`${params.category}`}</span>
+            <span className="my-quizzes-info_container_core-info-container-difficulty">{`${params.difficulty}`}</span>
+          </div>
+
+          <StatusElement
+            params={{
+              category: params.category,
+              difficulty: params.difficulty,
+            }}
+          />
+
+          <InfoContainer
+            params={{
+              category: params.category,
+              difficulty: params.difficulty,
+            }}
+            curQuiz={curQuiz}
+          />
+
+          <Link href="/my-quizzies" className="my-quizzes-info_container-link">
+            <HiArrowSmLeft />
+          </Link>
         </div>
-
-        <StatusElement
-          params={{
-            category: params.category,
-            difficulty: params.difficulty,
-          }}
+      )}
+      {curQuiz?.status === "finished" && (
+        <QuizSummary
+          points={curQuiz.scoredPoints}
+          quizzies={curQuiz.questions}
+          animation="false"
         />
-
-        <InfoContainer
-          params={{
-            category: params.category,
-            difficulty: params.difficulty,
-          }}
-        />
-
-        <Link href="/my-quizzies" className="my-quizzes-info_container-link">
-          <HiArrowSmLeft />
-        </Link>
-      </div>
+      )}
     </section>
   );
 };
