4a3503c815e53e9723c2803d9a6c7d4a37a3ca02
Biszku <dbichu@onet.pl>
2023-08-31 19:25:10 +0200

    removed fetch logic

--- components/Quiz-info/quiz-info-container.tsx
+++ components/Quiz-info/quiz-info-container.tsx
@@ -1,38 +1,35 @@
-"use client";
-import { useContext, FC } from "react";
-import { MainContext } from "../../context/context";
+import { FC } from "react";
 
 import ContinueElement from "./quiz-info-continue";
-import FinishedElement from "./quiz-info-finished";
 
 interface ParamsSlug {
   params: {
     category: string;
     difficulty: string;
   };
+  curQuiz:
+    | {
+        category: string;
+        difficulty: string;
+        status: string;
+        questions: any[];
+        numOfQuestion: number;
+        scoredPoints: number;
+      }
+    | undefined;
 }
 
-const InfoContainer: FC<ParamsSlug> = ({ params }) => {
-  const Context = useContext(MainContext);
-
-  const curQuiz = Context.quizzes.find(
-    (el) =>
-      el.category === params.category && el.difficulty === params.difficulty
-  );
-
+const InfoContainer: FC<ParamsSlug> = ({ params, curQuiz }) => {
   return (
-    <div className="my-quizzes-info_container_info-container">
-      {curQuiz?.status === "finished" && (
-        <FinishedElement quizziesQuestions={curQuiz.questions} />
-      )}
-      {curQuiz?.status === "unfinished" && (
+    curQuiz !== undefined && (
+      <div className="my-quizzes-info_container_info-container">
         <ContinueElement
           quizziesQuestions={curQuiz.questions}
           params={{ category: params.category, difficulty: params.difficulty }}
           numOfCurQuiz={curQuiz.numOfQuestion}
         />
-      )}
-    </div>
+      </div>
+    )
   );
 };
 export default InfoContainer;
