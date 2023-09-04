f704d0da241b2146e8af0fdf4562cf323094cc00
Biszku <dbichu@onet.pl>
2023-09-04 22:11:30 +0200

    little patch

---
+++ app/my-quizzies/quiz-info/[...id]/page.tsx
@@ -0,0 +1,16 @@
+import QuizInfo from "@/components/Quiz-info/quiz-info";
+import { FC } from "react";
+
+interface ParamsSlug {
+  params: {
+    id: string[];
+  };
+}
+
+const MyQuizInfo: FC<ParamsSlug> = ({ params }) => {
+  const [category, difficulty] = params.id;
+
+  return <QuizInfo params={{ category, difficulty }} />;
+};
+
+export default MyQuizInfo;
