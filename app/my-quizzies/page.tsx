8925937aaa9d0bc29aec56bd6df6cc54c4724903
Biszku <dbichu@onet.pl>
2023-09-04 22:11:12 +0200

    added navigation element

--- app/my-quizzies/page.tsx
+++ app/my-quizzies/page.tsx
@@ -1,7 +1,13 @@
-import MyQuizzesSection from "../../components/MyQuizzes/my-quizzes";
+import MyQuizzesSection from "@/components/MyQuizzes/my-quizzes";
+import MainNav from "@/components/Navigation/navigation";
 
 const myQuizzies = () => {
-  return <MyQuizzesSection />;
+  return (
+    <>
+      <MyQuizzesSection />
+      <MainNav />
+    </>
+  );
 };
 
 export default myQuizzies;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
-import Quizzies from "../components/Quizzies/quizzies";
+import MainNav from "@/components/Navigation/navigation";
+import Quizzies from "@/components/Quizzies/quizzies";
 
 const Home = () => {
   return (
-    <section className="general-container">
-      <Quizzies />
-    </section>
+    <>
+      <section className="general-container">
+        <Quizzies />
+      </section>
+      <MainNav />
+      
+    </>
   );
 };
 
