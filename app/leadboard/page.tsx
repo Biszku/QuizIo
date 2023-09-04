04bedf91f8a85c09942ad9784b9e71c21a2210c2
Biszku <dbichu@onet.pl>
2023-09-04 21:46:59 +0200

    deleted

--- app/leadboard/page.tsx
+++
@@ -1,7 +0,0 @@
-import LeadboardContent from "../../components/Leadboard/leadboard";
-
-const Leadboard = () => {
-  return <LeadboardContent />;
-};
-
-export default Leadboard;
--- app/user/profile/[id]/page.tsx
+++
@@ -1,5 +0,0 @@
-async function Test() {
-  return <h1>login</h1>;
-}
-
-export default Test;
