d2b62a32b45e34414f2af102f1bb21646c8da56c
Biszku <dbichu@onet.pl>
2023-09-07 14:30:44 +0200

    added some arguments

--- utils/getQuzzies.ts
+++ utils/getQuzzies.ts
@@ -1,15 +1,21 @@
-const GetQuzzies = async (
-  category: string = "",
-  difficulty: string = "easy",
-  limit: string = "20"
-) => {
+const GetQuzzies = async ({
+  category = "",
+  difficulty = "easy",
+  signal,
+  limit = 20,
+}: {
+  category: string;
+  difficulty: string;
+  limit?: number;
+  signal?: any;
+}) => {
   const req = await fetch(
     `https://quizapi.io/api/v1/questions?apiKey=${
       process.env.NEXT_PUBLIC_API_KEY
     }&category=${
       category === "random" ? "" : category
     }&difficulty=${difficulty}&limit=${limit}`,
-    { cache: "no-store" }
+    { signal }
   );
   const res = await req.json();
   return res;
